d2ffacfe2e2e72e07815e4e3eb6b61bfe38a5656
ELK169 <61375802+ELK169@users.noreply.github.com>
2020-02-29 15:32:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -206,7 +206,7 @@ class ElkHausnet extends utils.Adapter {
             //...
 
             // wenn es geklappt hat, dies melden:
-            this.setStateAsync(element.objname,element.state,true);
+            this.setStateAsync(element.obj,element.state,true);
 
             }
 
