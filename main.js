bc4b4ac15c6d0955b857400fbcb1a230128e3080
ELK169 <61375802+ELK169@users.noreply.github.com>
2020-03-07 14:24:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -356,7 +356,7 @@ if(obj==null)
 }
 else
 {
-            A.log.debug("Objekt geholt: "+err.stringify()+"  -  "+obj.stringify());
+            A.log.debug("Objekt geholt: "+obj.stringify());
 
 
             obj.native.AnzFehlerAktuell=0;
