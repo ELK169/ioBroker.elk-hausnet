4275bd1b7f28896470f28f96034516d0a6af0aec
ELK169 <61375802+ELK169@users.noreply.github.com>
2020-03-07 14:47:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -346,9 +346,9 @@ adapter.getStates('*', (err, states) =>
  A.log.info("Defaultwerte setzen...");
  Objekte.forEach(function(element) 
     {
-        A.log.debug("Objekt holen: "+ A.namespace+".Obj."+element.typ+"."+element.objname);
+        A.log.debug("Objekt holen: "+ "System."+A.namespace+".Obj."+element.typ+"."+element.objname);
 
-        A.getObject(A.namespace+".Obj."+element.typ+"."+element.objname, function(err,obj) 
+        A.getObject("System."+A.namespace+".Obj."+element.typ+"."+element.objname, function(err,obj) 
         {   
 if(obj==null || obj==undefined) 
 {
